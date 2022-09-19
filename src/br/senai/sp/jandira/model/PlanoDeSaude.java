95ef6177756e747f48ad7271676912cd197b0f4d
FELIPE DE OLIVEIRA FLORENCIO <22282108@CFP127ALUNO.LOCAL>
2022-09-19 11:17:53 -0300

    agenda e enum apreendidos

--- src/br/senai/sp/jandira/model/PlanoDeSaude.java
+++ src/br/senai/sp/jandira/model/PlanoDeSaude.java
@@ -4,27 +4,29 @@ public class PlanoDeSaude {
 
 	private String operadora;
 	private String tipoDoPlano;
-	
+
 	// Métodos de acesso
-	
+
 	public void setOperadora(String operadora) {
 		this.operadora = operadora;
 	}
-	
-	public String getOperadora(){
+
+	public String getOperadora() {
 		return this.operadora;
 	}
-	
+
 	public void setTipoDoPlano(String tipoDoPlano) {
 		this.tipoDoPlano = tipoDoPlano;
 	}
-	
+
 	public String getTipoDoPlano() {
 		return this.tipoDoPlano;
 	}
 	
-	public String getDadosDoPlano(){
+	
+
+	public String getDadosDoPlano() {
 		return "Plano: " + this.operadora + ", " + this.tipoDoPlano;
-		
+
 	}
 }
